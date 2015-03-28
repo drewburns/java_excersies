1d51b4c7de3cd445abba0515a08159f25ed99fbd
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-03-28 14:57:47 -0400

    second try

--- animals.js
+++ animals.js
@@ -4,32 +4,43 @@
 function Animal (name, legs) {
     this.name = name;
     this.legs = legs;
+};
 
-    this.identify = function() {
-        return "I am a " + name + "with " + legs + " legs.";
-    };
-}
+Animal.prototype.identify = function() {
+        return "I am a " + this.name + "with " + this.legs + " legs.";
+};
 function Zoo (animals) {
-    this.animals = animals;
+    init : function(animals){
+      this.animals = animals;
+    },
+};
+var Zoo = {
+    init : function(animals){
+      this.animals = animals;
+    },
 
-    this.bipeds = function() {
-      var bipedArray = [] ;
-      animals.forEach(function(animal) {
-          if(animal.legs == 2) { bipedArray.push(animal) ; }
+    bipeds : function() {
+      return this.animals.filter(function(animal){
+        return animal.legs === 2;
       });
-      return bipedArray;
-    };
+      // var bipedArray = [] ;
+      // animals.forEach(function(animal) {
+      //     if(animal.legs == 2) { bipedArray.push(animal) ; }
+      // });
+      // return bipedArray;
+    },
 
-    this.quadrupeds = function() {
-      var quadArray = [] ;
-      animals.forEach(function(animal) {
-        if(animal.legs == 4) { quadArray.push(animal) ; }
+    quadrupeds : function() {
+      return this.animals.filter(function(animal){
+        return animal.legs === 4;
       });
-      return quadArray;
-    };
-
-
-}
+    }
+      // var quadArray = [] ;
+      // animals.forEach(function(animal) {
+      //   if(animal.legs == 4) { quadArray.push(animal) ; }
+      // });
+      // return quadArray;
+};
 
 
 
@@ -51,19 +62,20 @@ var animals = [
   new Animal("Centipede", 100)
 ];
  
-var tolland = new Zoo(animals);
+Zoo.init(animals);
+
 assert(
-  tolland.bipeds().length === 3, "the Zoo should have 3 bipeds"
+  Zoo.bipeds().length === 3, "the Zoo should have 3 bipeds"
 );
 assert(
-  tolland.quadrupeds().length === 2, "the Zoo should have 2 bipeds"
+  Zoo.quadrupeds().length === 2, "the Zoo should have 2 bipeds"
 );
 assert(
-  animals[0].identify() === "I am a Human with 2 legs.", "humans have 2 legs"
+  Zoo[0].identify() === "I am a Human with 2 legs.", "humans have 2 legs"
 );
 assert(
-  animals[2].name === "Kangaroo", "expected 'Kangaroo'"
+  Zoo[2].name === "Kangaroo", "expected 'Kangaroo'"
 );
 assert(
-  animals[0].identify === animals[5].identify, "only one implementation of the identify() function should exist"
+  Zoo[0].identify === animals[5].identify, "only one implementation of the identify() function should exist"
 );
--- orange.js
+++ orange.js
@@ -1,50 +1,165 @@
+//-------------------------------------------------------------------------
+// MODEL
+//-------------------------------------------------------------------------
+ 
+//-------------------------------------
+// Parent-class Tree:
+ 
 
-
-function Grove (theTrees) {
-  this.trees = theTrees;
+function Tree() {
+  this.age = 0
+  this.height = 0
+  this.fruit_count = 0
+  this.type = "Fruit"
+  this.lifeSpan = 30
+  this.maturity = 12;
 }
-function Tree (name) {
-    this.name = name;
-    this.fruits = 0;
-    this.height = 5;
-}
-
-
+ 
+function Fruit(type) {
+  this.type = type
+};
+ 
+Tree.prototype.year = function(){
+  if (this.dead() ){
+    this.fruitCount = 0;
+    return "This tree is dead. Plant another.";
+  }
+  else if (this.age < this.maturity){
+    this.fruitCount = 20;
+    this.height += 1;
+    this.age += 1;
+  }
+  else {
+    this.fruitCount = 10;
+    this.age += 1; }
+};
+ 
+Tree.prototype.dead = function(){
+  if (this.age > this.lifeSpan){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+ 
+Tree.prototype.pickFruit = function(basket){
+  if (this.fruitCount > 0){
+    this.fruitCount -= 1;
+    var type = this.type;
+    basket.push(new Fruit(type));
+  }
+};
+ 
+//-------------------------------------
+// Sub-classes of Tree:
+ 
+function OrangeTree(){
+  this.type = "orange";
+  this.lifeSpan = 23;
+  this.maturity = 8
+};
+ 
+OrangeTree.prototype = new Tree;
+ 
+ 
+function AppleTree(){
+  this.type = "apple"
+  this.lifeSpan = 19;
+  this.maturity = 9
+};
+ 
+AppleTree.prototype = new Tree;
+ 
+ 
+function MangoTree(){
+  this.type = "mango"
+  this.lifeSpan = 16;
+  this.maturity = 10
+};
+ 
+MangoTree.prototype = new Tree;
+ 
+ 
+//-------------------------------------
+// Literal constructor for Grove:
+ 
+var Grove = {
+ 
+  init : function(trees){
+    this.trees = trees;
+  },
+ 
+  year : function(){
+    this.trees.forEach(function(tree){
+      tree.year();
+    });
+  },
+ 
+  allTrees : function(){
+    return this.trees
+  },
+ 
+  matureTrees : function(){
+    return this.trees.filter(function(tree){
+      return tree.age >= tree.maturity;
+    });
+  },
+ 
+  deadTrees : function(){
+    return this.trees.filter(function(tree){
+      return tree.age >= tree.lifeSpan;
+    });
+  }
+ 
+};
+ 
+ 
 //-------------------------------------------------------------------------
 // ORANGE TREE DRIVER CODE
 //-------------------------------------------------------------------------
-
+ 
 function fastforward(theTree, years){
   i = 0
   do {
     i += 1;
     theTree.year();
-    console.log('The '+theTree.type +'tree is '+ theTree.age + 'years old, and is '+ theTree.height +' feet tall.')
+    console.log('The '+theTree.type +'tree is '+ theTree.age + ' years old, and is '+ theTree.height +' feet tall.')
     basket = []
     var fruits = theTree.fruitCount;
       while (fruits > 0) {
         fruits -= 1 ;
         theTree.pickFruit(basket);
-
+ 
       } 
     console.log('You put '+ basket.length +' fruits into your basket')
   } while (i < years); 
 };
-
-
+ 
+// tree = new OrangeTree;
+// fastforward(tree, 24);
+// basket.length;
+// basket[0].type;
+// tree.dead();
+ 
 //-------------------------------------------------------------------------
 // GROVE DRIVER CODE
 //-------------------------------------------------------------------------
-
-var treeArray = [new Tree("OrangeTree"),
-         new Tree("AppleTree"),
-         new Tree("MangoTree")]
-
-var andrewsGrove = new Grove(treeArray)
-
-var length = andrewsGrove.trees.length;
-
+ 
+trees = [new OrangeTree,
+         new AppleTree,
+         new MangoTree]
+ 
+Grove.init(trees)
+ 
+var length = Grove.trees.length;
+ 
 for (var i = 0; i < length; i++ ) {
   element = Grove.trees[i];
   fastforward(element, 20);
 };
+ 
+// Grove.year();
+// Grove.allTrees();
+// Grove.matureTrees();
+// Grove.deadTrees();
